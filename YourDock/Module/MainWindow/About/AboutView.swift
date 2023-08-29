f77741088cd688fe6dd3dc71d422e4805ca66aa2
nhiroyasu <nhiro1109pri@gmail.com>
2023-08-29 11:01:52 +0900

    add github icon

--- YourDock/Module/MainWindow/About/AboutView.swift
+++ YourDock/Module/MainWindow/About/AboutView.swift
@@ -8,6 +8,23 @@ struct AboutView: View {
                     .resizable()
                     .frame(width: 64, height: 64)
                 Text(applicationName())
+                Button {
+                    NSWorkspace.shared.open(Constants.githubUrl)
+                } label: {
+                    Image(nsImage: Constants.githubIcon)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                }
+                .buttonStyle(.plain)
+                .onHover { inside in
+                    if inside {
+                        NSCursor.pointingHand.push()
+                    } else {
+                        NSCursor.pop()
+                    }
+                }
+                .padding(8)
             }
             Text(AppText.copyRight)
         }
